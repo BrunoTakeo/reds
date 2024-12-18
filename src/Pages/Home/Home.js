3d7de13522974f4a42f2468120f3f9f8d0f53e4d
BrunoTakeo <brunotakeo14@gmail.com>
2024-12-19 04:03:39 +0900

    Inicio Da Customização Da Pagina Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,28 +1,31 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import './home.css'
 
 const Home = () => {
   return (
     <Fragment>
-        <div className='Bem_Vindo'>
-          <p>Bem-Vindo Ao<b/>Reds</p>
-        </div>
-        <div>
-          <Link>Veja Mais</Link>
-          <i></i>
-        </div>
-        <div>
-          <div>
-            <img/>
-            <h3>Como <br/> Jogar?</h3>
+      <div className='Page_Home'>
+          <div className='Bem_Vindo'>
+            <p>Bem-Vindo Ao<b/>Reds</p>
           </div>
-        </div>
-        <div>
-          <span>
-            Pronto Para Começar?
-          </span>
-          <Link>Jogar agora</Link>
-        </div>
+          <div>
+            <Link>Veja Mais</Link>
+            <i></i>
+          </div>
+          <div>
+            <div>
+              <img/>
+              <h3>Como <br/> Jogar?</h3>
+            </div>
+          </div>
+          <div>
+            <span>
+              Pronto Para Começar?
+            </span>
+            <Link>Jogar agora</Link>
+          </div>
+      </div>
     </Fragment>
   )
 }
