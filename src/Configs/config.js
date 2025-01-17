e20cd66b40fad313235db52657243c61fcab4cbe
John <joao.piva@aluno.ifsp.edu.br>
2025-01-17 01:30:23 -0300

    introdução da página 'sala'

--- src/Configs/config.js
+++ src/Configs/config.js
@@ -7,6 +7,9 @@ import IMAGEM_WELCOME_BANNER from '../assets/img/welcome_banner.png'
 import IMAGEM_VER_MAIS from '../assets/img/arrow_bottom.svg'
 import IMAGEM_COMO_JOGAR from '../assets/img/tutorial_img.png'
 import IMAGEM_TOPICO_HOME from '../assets/img/topico.svg'
+import IMAGEM_ARROW_LEFT from '../assets/img/arrow-left.svg'
+import IMAGEM_ARROW_RIGHT from '../assets/img/arrow-rigth.svg'
+import IMAGEM_PLUS from '../assets/img/plus.svg'
 
 export const IMAGEM_LOGO_W = IMAGEM_ICON_REDS_WHITE;
 export const IMAGEM_LOGO_B = IMAGEM_ICON_REDS_BLACK;
@@ -17,3 +20,6 @@ export const IMAGEM_WELCOME = IMAGEM_WELCOME_BANNER;
 export const IMAGEM_VER = IMAGEM_VER_MAIS;
 export const IMAGEM_TUTORIAL_JOGO = IMAGEM_COMO_JOGAR;
 export const IMAGEM_TOPICO = IMAGEM_TOPICO_HOME;
+export const IMAGEM_SETA_ESQ = IMAGEM_ARROW_LEFT;
+export const IMAGEM_SETA_DIR = IMAGEM_ARROW_RIGHT;
+export const IMAGEM_MAIS = IMAGEM_PLUS;
--- src/Pages/Sala/Sala.js
+++ src/Pages/Sala/Sala.js
@@ -1,81 +1,52 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import './sala.css'
-import { IMAGEM_TOPICO, IMAGEM_TUTORIAL_JOGO, IMAGEM_VER } from '../../Configs/config'
+import { IMAGEM_SETA_ESQ, IMAGEM_SETA_DIR, IMAGEM_MAIS } from '../../Configs/config'
 
 const Home = () => {
   return (
-    <Fragment>
-      <div className='home-container'>
-          <div className='bem-vindo'>
-            <div className='bem-vindo-conteudo'>
-              <p>Bem-vindo ao <br></br>REDS</p>
-            </div>
+    <>
+      <div className='sala-container'>
+          <div className='criar-sala'>
+              <img src={IMAGEM_MAIS}/>
+              <p>Crie uma sala e divirta-se</p>
           </div>
-          <div className='veja-mais'>
-            <p>Veja mais</p>
-            <img src={IMAGEM_VER}/>
-          </div>
-          <div className='como-jogar'>
-            <div className='logo-tutorial'>
-              <img src={IMAGEM_TUTORIAL_JOGO}/>
-              <h1>COMO JOGAR?</h1>
-            </div>
-            <div className='como-jogar-conteudo'>  
-              <div className='topico-container'>
-                  <div className='topico'>
-                      <div className='topico-icon'>
-                          <img src={IMAGEM_TOPICO}/>
+          <div className='sala-carrossel'>
+              <div className='carrossel-titulo'>
+                <p>Salas disponíveis</p>
+              </div>
+              <div className='carrossel-salas'>
+                <div className='carrossel-sala'>
+                  <div className='sala-titulo'>
+                    <p>Estudos de POO</p>
+                    <img className='adicionar-sala' src={IMAGEM_MAIS}/>
+                  </div>
+                  <div className='sala-conteudo'>
+                      <div className='sala-infos'>
+                        <div className='sala-categoria'>
+                          <p>Lógica, Estrutura de Dados</p>
+                        </div>
+                        <div className='sala-tipo-pergunta'>
+                          <p>Verdadeiro e Falso</p>
+                        </div>
                       </div>
-                      <div className='texto'>
-                          <p>O jogo se trata de um quiz competitivo para testar os conhecimentos de seus participantes sobre a área de TI.</p>
+                      <div className='sala-jogadores'>
+                          <p>3 / 5</p>
                       </div>
                   </div>
-                  <div className='topico'>
-                      <div className='topico-icon'>
-                        <img src={IMAGEM_TOPICO}/>
-                      </div>
-                      <div className='texto'>
-                          <p>Encontre salas disponíveis ou crie a sua própria para conseguir o código de acesso.</p>
-                      </div>
-                  </div>
-                  <div className='topico'>
-                      <div className='topico-icon'>
-                        <img src={IMAGEM_TOPICO}/>
-                      </div>
-                      <div className='texto'>
-                          <p>Uma vez na sala, coloque em prática todo seu saber para sobreviver a cada resposta correta.</p>
-                      </div>
-                  </div>
-                  <div className='topico'>
-                      <div className='topico-icon'>
-                        <img src={IMAGEM_TOPICO}/>
-                      </div>
-                      <div className='texto'>
-                          <p>Se errar, seus pontos de vida decaem correspondente ao seu tempo de resposta em relação aos demais jogadores.</p>
-                      </div>
-                  </div>
-                  <div className='topico'>
-                      <div className='topico-icon'>
-                        <img src={IMAGEM_TOPICO}/>
-                      </div>
-                      <div className='texto'>
-                          <p>Sem um limite de perguntas, o jogo só acaba quando restar apenas um jogador sem seus pontos zerados.</p>
-                      </div>
-                  </div>
-              </div>          
-            </div>
-          </div>
-          <div className='cti'>
-            <span>
-              Pronto para começar ?
-            </span>
-            <Link className='jogo-botao' to='/sala'>
-              Jogar agora
-            </Link>
+                </div>
+              </div>
+              <div className='carrossel-slider'>
+                <div className='carrossel-ant'>
+                  <img src={IMAGEM_SETA_ESQ}/>
+                </div>
+                <div className='carrossel-prox'>
+                  <img src={IMAGEM_SETA_DIR}/>
+                </div>
+              </div>
           </div>
       </div>
-    </Fragment>
+    </>
   )
 }
 
