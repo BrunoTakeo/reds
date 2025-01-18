dff6a676bcef34242c8d101e9bdff26e5fcbe363
John <joao.piva@aluno.ifsp.edu.br>
2025-01-18 17:21:31 -0300

    página da sala responsivo

--- src/Pages/Sala/Sala.js
+++ src/Pages/Sala/Sala.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './sala.css'
+import React, { useRef } from 'react';
+import { anterior, proximo } from './Slider';
 import { IMAGEM_SETA_ESQ, IMAGEM_SETA_DIR, IMAGEM_MAIS } from '../../Configs/config'
+import './sala.css'
 
-const Home = () => {
+const Sala = () => {
+  const sliderRef = useRef(null);
+
   return (
     <>
       <div className='sala-container'>
           <div className='criar-sala'>
-              <img src={IMAGEM_MAIS}/>
-              <p>Crie uma sala e divirta-se</p>
+            <div className='criar-sala-logo'>
+              <svg width="30" height="30" viewBox="0 0 33 33" stroke='currentColor' fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11.592 30.832V0.368H21.448V30.832H11.592ZM0.712 20.272V10.928H32.264V20.272H0.712Z"/>
+              </svg>
+            </div>
+            <p>Crie uma sala e divirta-se</p>
           </div>
           <div className='sala-carrossel'>
               <div className='carrossel-titulo'>
                 <p>Salas disponíveis</p>
               </div>
-              <div className='carrossel-salas'>
+              <div className='carrossel-salas' ref={sliderRef}>
+                
+                {/* 1 */}
                 <div className='carrossel-sala'>
                   <div className='sala-titulo'>
                     <p>Estudos de POO</p>
@@ -35,12 +43,82 @@ const Home = () => {
                       </div>
                   </div>
                 </div>
+                
+                {/* 2 */}
+                <div className='carrossel-sala'>
+                  <div className='sala-titulo'>
+                    <p>Sala do Renatão</p>
+                    <img className='adicionar-sala' src={IMAGEM_MAIS}/>
+                  </div>
+                  <div className='sala-conteudo'>
+                      <div className='sala-infos'>
+                        <div className='sala-categoria'>
+                          <p>Sistemas Operacionais</p>
+                        </div>
+                        <div className='sala-tipo-pergunta'>
+                          <p>Ambos</p>
+                        </div>
+                      </div>
+                      <div className='sala-jogadores'>
+                          <p>2 / 5</p>
+                      </div>
+                  </div>
+                </div>
+                
+                {/* 3 */}
+                <div className='carrossel-sala'>
+                  <div className='sala-titulo'>
+                    <p>Estudos de POO</p>
+                    <img className='adicionar-sala' src={IMAGEM_MAIS}/>
+                  </div>
+                  <div className='sala-conteudo'>
+                      <div className='sala-infos'>
+                        <div className='sala-categoria'>
+                          <p>Lógica, Estrutura de Dados</p>
+                        </div>
+                        <div className='sala-tipo-pergunta'>
+                          <p>Verdadeiro e Falso</p>
+                        </div>
+                      </div>
+                      <div className='sala-jogadores'>
+                          <p>3 / 5</p>
+                      </div>
+                  </div>
+                </div>
+                
+                {/* 4 */}
+                <div className='carrossel-sala'>
+                  <div className='sala-titulo'>
+                    <p>Estudos de POO</p>
+                    <img className='adicionar-sala' src={IMAGEM_MAIS}/>
+                  </div>
+                  <div className='sala-conteudo'>
+                      <div className='sala-infos'>
+                        <div className='sala-categoria'>
+                          <p>Lógica, Estrutura de Dados</p>
+                        </div>
+                        <div className='sala-tipo-pergunta'>
+                          <p>Verdadeiro e Falso</p>
+                        </div>
+                      </div>
+                      <div className='sala-jogadores'>
+                          <p>3 / 5</p>
+                      </div>
+                  </div>
+                </div>
+                
               </div>
               <div className='carrossel-slider'>
-                <div className='carrossel-ant'>
+                <div 
+                  className='carrossel-ant' 
+                  onClick={() => anterior(sliderRef.current)}
+                >
                   <img src={IMAGEM_SETA_ESQ}/>
                 </div>
-                <div className='carrossel-prox'>
+                <div 
+                  className='carrossel-prox' 
+                  onClick={() => proximo(sliderRef.current)}
+                >
                   <img src={IMAGEM_SETA_DIR}/>
                 </div>
               </div>
@@ -50,4 +128,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Sala
